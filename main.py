6d51d180afb9bc5473be9fb0264a20938b96992d
gamexdifficulty <gamexdifficulty@yahoo.com>
2026-05-25 18:24:50 +0200

    Added main menu

--- main.py
+++ main.py
@@ -1,19 +1,85 @@
 from frostlight_engine import *
 
+from data.classes.island import Island
+from data.classes.button import Button
+from data.classes.player_manager import PlayerManager
+from data.classes.gameManager import GameManager
+
 class Game(FrostlightEngine):
     def __init__(self):
         super().__init__(canvas_size=[320,180],fps_limit=165)
         self.running = True
-        self.state = "game"
-    
+        self.transition = False
+        self.state = "menu"
+
+        self.transition_unit = 1.0
+        self.mouse_pos = [0,0]
+
+        self.input.bind("accept",MOUSE.LEFT,CLICKED)
+        self.input.bind("accept",KEYBOARD.ENTER,PRESSED)
+        
+        self.input.bind("menu",KEYBOARD.ESCAPE,CLICKED)
+
+        self.mouse_sprite = Sprite("mouse.png")
+
+        self.multiplayer_button = Button(self, pos=[144,64], sprite="button-multiplayer.png", callback=self.button_multiplayer)
+        self.singleplayer_button = Button(self, pos=[144,84], sprite="button-singleplayer.png", callback=self.button_singleplayer)
+        self.leave_button = Button(self, pos=[144,104], sprite="button-leave.png", callback=self.button_quit)
+        
+        self.gameManager = GameManager(self)
+        
+        self.island = Island(self)
+        self.player_manager = PlayerManager(self)
+        
     def event_quit(self):
         self.running = False
+
+    def begin_transition(self):
+        self.transition = True
+        
+    def button_quit(self):
+        self.running = False
+        exit(0)
+    
+    def button_multiplayer(self):
+        self.begin_transition()
+    
+    def button_singleplayer(self):
+        self.begin_transition()
     
     def update(self):
-        ...
+        self.mouse_pos = [(self.input.mouse.position[0]/self.window.window_size[0])*self.window.canvas_size[0], 
+                     (self.input.mouse.position[1]/self.window.window_size[1])*self.window.canvas_size[1]]
+        
+        if self.transition:
+            self.transition_unit = max(0.0, self.transition_unit - 1.0*self.delta_time)
+            if self.transition_unit == 0.0:
+                self.state = "game"
+                self.transition = False
+                self.transition_unit = 1.0
+
+        self.island.update()
+
+        if self.state == "menu":
+            self.multiplayer_button.update(self.mouse_pos, self.transition_unit)
+            self.singleplayer_button.update(self.mouse_pos, self.transition_unit)
+            self.leave_button.update(self.mouse_pos, self.transition_unit)
+            self.mouse_sprite.alpha = self.transition_unit
+        
+        elif self.state == "game":
+            self.gameManager.update()
+        
+        if self.input.get("menu"):
+            self.state = "menu"
 
     def draw(self):
-        self.window.fill(200,255,255)
+        self.island.draw()
+
+        if self.state == "menu":
+            self.multiplayer_button.draw()
+            self.singleplayer_button.draw()
+            self.leave_button.draw()
+            self.window.render(self.mouse_sprite, self.mouse_pos)
 
 game = Game()
 game.run()
