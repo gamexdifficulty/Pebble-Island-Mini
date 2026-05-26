dbb069395765fd1c3e215d5299763c46bf9daaee
gamexdifficulty <gamexdifficulty@yahoo.com>
2026-05-26 04:10:01 +0200

    Moved weather system to game manager

--- data/classes/gameManager.py
+++ data/classes/gameManager.py
@@ -1,21 +1,27 @@
-import time
+import opensimplex
 from frostlight_engine import *
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from main import Game
-    
+
+SCALE = 0.0008
+
 class GameManager:
     def __init__(self,game:"Game"):
         self.game = game
         
-        self.time = 0
+        self.weather_value = 0
+        self.time = 500
         self.day = 1
-    
+
+        opensimplex.seed(933673157426)
+        
     def update(self):
+        self.weather_value = opensimplex.noise2((self.day*2400+self.time)*SCALE,1*SCALE)
+        
         self.time += 8*self.game.delta_time
         if self.time >= 2400:
             self.time -= 2400
             self.day += 1
             if self.day >= 31:
                 self.day = 1
-        print(self.time, self.day)
